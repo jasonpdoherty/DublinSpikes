0e93916079070d99ff2c7652704e25f9b6dd1205
Jason Doherty <jasonpdoherty@googlemail.com>
2020-01-07 11:01:32 +0000

    Scaffolding

---
+++ Calculator/Calculator/Calculator.Core/StringParser.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Calculator.Core
+{
+    public static class StringParser
+    {
+        public static IEnumerable<string> HandleBrackets(string userInputs)
+        {
+            List<string> result = new List<string>();
+
+            string section = "";
+            
+            foreach (var item in userInputs.Reverse())
+            {
+                if (item == char.Parse(")"))
+                {
+                    result.Add(section.Reverse().ToString());
+                    section = string.Empty;
+
+                }
+
+                section += item;
+            }
+
+            result.Add(Reverse(section)); // last section
+            return result;    
+        }
+
+        private static string Reverse(string text)
+        {
+            if (text == null) return null;
+
+            // this was posted by petebob as well 
+            char[] array = text.ToCharArray();
+            Array.Reverse(array);
+            return new String(array);
+        }
+    }    
+}
---
+++ Calculator/Calculator/CalculatorTests/StringParserTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+
+
+using Xunit;
+
+using Calculator.Core;
+
+namespace CalculatorTests
+{
+    public class StringParserTests
+    {
+        [Theory]
+        [InlineData("2+3")]
+        public void HandleBrackets_NoBrackets_Success(string userInput)
+        {
+            var result = StringParser.HandleBrackets(userInput);
+
+            Assert.Single(result);
+            Assert.Equal(userInput, result.ElementAt(0));
+        }
+
+        [Theory]
+        [InlineData("(2+3)")]
+        [InlineData("(2+4)")]
+        [InlineData("(2+4) * 2")]
+
+        public void HandleBrackets_1SetOfBracketsBrackets_Success(string userInput)
+        {
+
+        }
+
+        [Theory]
+        [InlineData("((2+3))")]
+        public void HandleBrackets_MultipleBracketsUsed_ErrorThrown(string userInput)
+        {
+
+        }
+    }
+}
